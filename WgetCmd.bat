5a097b1230bb0ca5b8777e54c1fc63e9902dafc6
JuanElBueno <85785935+JuanElBueno@users.noreply.github.com>
2021-08-26 23:26:09 +0200

    Add files via upload

---
+++ WgetCmd.bat
@@ -0,0 +1,12 @@
+@echo off
+IF NOT EXIST C:\Windows\System32\wget.exe goto 32o64
+:32o64
+if $SYSTEM_os_arch==x86 (
+cd C:\Windows\System32
+powershell -command iwr 'https://eternallybored.org/misc/wget/1.21.1/64/wget.exe' -OutFile 'wget.exe'
+echo Usando 64bits & timeout /T 3s >nul
+) else (
+cd C:\Windows\System32
+powershell -command iwr 'https://eternallybored.org/misc/wget/1.21.1/32/wget.exe' -OutFile 'wget.exe'
+echo Usando 32bits & timeout /T 3 >nul
+)
