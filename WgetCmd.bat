7ee9a4a91887a95fc599b32479b9a672229ae052
JuanElBueno <85785935+JuanElBueno@users.noreply.github.com>
2021-08-26 23:38:35 +0200

    Update WgetCmd.bat

--- WgetCmd.bat
+++ WgetCmd.bat
@@ -3,10 +3,10 @@ IF NOT EXIST C:\Windows\System32\wget.exe goto 32o64
 :32o64
 if $SYSTEM_os_arch==x86 (
 cd C:\Windows\System32
-powershell -command iwr 'https://eternallybored.org/misc/wget/1.21.1/64/wget.exe' -OutFile 'wget.exe'
-echo Usando 64bits & timeout /T 3s >nul
-) else (
-cd C:\Windows\System32
 powershell -command iwr 'https://eternallybored.org/misc/wget/1.21.1/32/wget.exe' -OutFile 'wget.exe'
 echo Usando 32bits & timeout /T 3 >nul
+) else (
+cd C:\Windows\System32
+powershell -command iwr 'https://eternallybored.org/misc/wget/1.21.1/64/wget.exe' -OutFile 'wget.exe'
+echo Usando 64bits & timeout /T 3s >nul
 )
