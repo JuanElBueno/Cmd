5b273400d35df3121e33cbb1cad6fabdaae2e169
JuanElBueno <85785935+JuanElBueno@users.noreply.github.com>
2021-12-13 21:19:10 +0100

    Add files via upload

--- WgetCmd.bat
+++ WgetCmd.bat
@@ -1,13 +1,39 @@
 @echo off
-IF EXIST C:\Windows\System32\wget.exe echo Execuction Exitosa & echo [+] Salendo... & timeout /T 2 >nul & Exit
-IF NOT EXIST C:\Windows\System32\wget.exe goto 32o64
+color 9f
+
+set Version=V1.0
+
+if $SYSTEM_os_arch==x86 (
+  set Titulo=Juan El Bueno %Version% (32 bits)
+) else (
+  set Titulo=Juan El Bueno %Version% (64 bits)
+)
+
+
+IF EXIST C:\Windows\System32\wget.exe ( 
+echo Execuction Exitosa
+echo.
+echo [+] Salendo...
+timeout /T 3 >nul 
+Exit
+) else (
+goto 32o64
+)
 :32o64
 if $SYSTEM_os_arch==x86 (
 cd C:\Windows\System32
 powershell -command iwr 'https://eternallybored.org/misc/wget/1.21.1/32/wget.exe' -OutFile 'wget.exe'
-echo Usando 32bits & timeout /T 3 >nul & echo [+] Salendo... & timeout /T 2 >nul & Exit
+echo Usando 32bits 
+echo.
+echo [+] Salendo...
+timeout /T 6 >nul 
+Exit
 ) else (
 cd C:\Windows\System32
 powershell -command iwr 'https://eternallybored.org/misc/wget/1.21.1/64/wget.exe' -OutFile 'wget.exe'
-echo Usando 64bits & timeout /T 3 >nul & echo [+] Salendo... & timeout /T 2 >nul & Exit
+echo Usando 64bits
+echo.
+echo [+] Salendo... 
+timeout /T 6 >nul 
+Exit
 )
