cbb81da08ccad805b30cdd1f2d45d02628eb2aad
JuanElBueno <85785935+JuanElBueno@users.noreply.github.com>
2022-03-29 04:36:03 +0200

    Add files via upload

--- WgetCmd.bat
+++ WgetCmd.bat
@@ -1,7 +1,7 @@
 @echo off
 color 9f
 
-set Version=V1.0
+set Version=V1.3
 
 if $SYSTEM_os_arch==x86 (
   set Titulo=Juan El Bueno %Version% (32 bits)
@@ -22,7 +22,7 @@ goto 32o64
 :32o64
 if $SYSTEM_os_arch==x86 (
 cd C:\Windows\System32
-powershell -command iwr 'https://eternallybored.org/misc/wget/1.21.1/32/wget.exe' -OutFile 'wget.exe'
+powershell -command iwr 'https://web.archive.org/web/20211101121933if_/https://eternallybored.org/misc/wget/1.21.2/32/wget.exe' -OutFile 'wget.exe'
 echo Usando 32bits 
 echo.
 echo [+] Salendo...
@@ -30,7 +30,7 @@ timeout /T 6 >nul
 Exit
 ) else (
 cd C:\Windows\System32
-powershell -command iwr 'https://eternallybored.org/misc/wget/1.21.1/64/wget.exe' -OutFile 'wget.exe'
+powershell -command iwr 'https://web.archive.org/web/20220218172657if_/https://eternallybored.org/misc/wget/1.21.2/64/wget.exe' -OutFile 'wget.exe'
 echo Usando 64bits
 echo.
 echo [+] Salendo... 
