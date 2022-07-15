edaaca3998dbc79116d18048036e7ecf793599e8
JuanElBueno <85785935+JuanElBueno@users.noreply.github.com>
2022-07-15 13:20:09 +0200

    Add files via upload

--- WgetCmd.bat
+++ WgetCmd.bat
@@ -1,14 +1,18 @@
 @echo off
 color 9f
 
-set Version=V1.3
+set Version=V1.3.9
 
-if $SYSTEM_os_arch==x86 (
+ping -n 1 8.8.8.8
+if %ERRORLEVEL% == 1 goto sinconexioni
+
+:titulot
+
+if "%PROCESSOR_ARCHITECTURE%"=="x86" (
   set Titulo=Juan El Bueno %Version% (32 bits)
 ) else (
   set Titulo=Juan El Bueno %Version% (64 bits)
 )
-
 
 IF EXIST C:\Windows\System32\wget.exe ( 
 echo Execuction Exitosa
@@ -20,7 +24,7 @@ Exit
 goto 32o64
 )
 :32o64
-if $SYSTEM_os_arch==x86 (
+if "%PROCESSOR_ARCHITECTURE%"=="x86" (
 cd C:\Windows\System32
 powershell -command iwr 'https://web.archive.org/web/20211101121933if_/https://eternallybored.org/misc/wget/1.21.2/32/wget.exe' -OutFile 'wget.exe'
 echo Usando 32bits 
@@ -37,3 +41,15 @@ echo [+] Salendo...
 timeout /T 6 >nul 
 Exit
 )
+
+:sinconexioni
+set sinconexiona=No tienes internet
+mode con: cols=52 lines=18 
+cls
+echo *************************************************
+echo.
+echo * No tienes internet vuelve intentalo mas tarde *
+echo.
+echo *************************************************
+timeout /T 5 >nul
+goto titulot
