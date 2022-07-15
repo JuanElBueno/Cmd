b82a102973ea727bcea5ff117f1252a7bd407659
JuanElBueno <85785935+JuanElBueno@users.noreply.github.com>
2022-07-15 13:25:52 +0200

    Add files via upload

--- Comandos.bat
+++ Comandos.bat
@@ -3,7 +3,7 @@
 color 9f
 REM Ruta general
 set Beta=Alfa
-set Version=V2.4.2
+set Version=V2.4.2.2
 set ruta=C:\Juanelbuenocopiadelosarcivos
 set programas=%ruta%\programas
 set admin=%ruta%\admin
@@ -123,13 +123,13 @@ goto menu
 ) else (
 if "%PROCESSOR_ARCHITECTURE%"=="x86" (
 cd %ruta%
-powershell -command iwr 'https://web.archive.org/web/20211101121933if_/https://eternallybored.org/misc/wget/1.21.2/32/wget.exe' -OutFile 'wget.exe'
+powershell -command iwr 'https://eternallybored.org/misc/wget/1.21.3/32/wget.exe' -OutFile 'wget.exe'
 echo.
 timeout /T 6 >nul
 goto menu
 ) else (
 cd %ruta%
-powershell -command iwr 'https://web.archive.org/web/20220218172657if_/https://eternallybored.org/misc/wget/1.21.2/64/wget.exe' -OutFile 'wget.exe' 
+powershell -command iwr 'https://eternallybored.org/misc/wget/1.21.3/64/wget.exe' -OutFile 'wget.exe' 
 echo. 
 timeout /T 6 >nul 
 goto menu
